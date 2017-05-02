0521351e87fa2564204f9a31f18ff2eead52cbfd
Traceur Lou <traceurlou@gmail.com>
2017-05-02 11:38:31 +0800

    using getElementsByClassName() instead of querySelectorAll()

--- src/views/js/main.js
+++ src/views/js/main.js
@@ -488,7 +488,7 @@ function updatePositions() {
   frame++;
   window.performance.mark("mark_start_frame");
 
-  var items = document.querySelectorAll('.mover');
+  var items = document.getElementsByClassName('mover');
   var scrollTop = document.body.scrollTop / 1250;
   for (var i = 0; i < items.length; i++) {
     var phase = Math.sin(scrollTop + (i % 5));
